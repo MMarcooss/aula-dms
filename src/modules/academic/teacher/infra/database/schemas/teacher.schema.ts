81d269608e17d8d9d86dd631bfd9c6a12047b9d1
MarcosVTavares <marquinhotavares03@gmail.com>
2026-03-12 19:13:57 -0300

    adicionado
    Co-Authored-By: Vitor-Calliari <163209006+vitor-calliari@users.noreply.github.com>
    Co-Authored-By: MatheusFQueiroz <125527991+matheusfqueiroz@users.noreply.github.com>
    Co-Authored-By: Matheus Giombelli <87329314+matheusgiombelli@users.noreply.github.com>
    Co-Authored-By: carolinaMichgorr <146143401+carolinamichgorr@users.noreply.github.com>

--- src/modules/academic/teacher/infra/database/schemas/teacher.schema.ts
+++ src/modules/academic/teacher/infra/database/schemas/teacher.schema.ts
@@ -4,8 +4,11 @@ export const teachersSchema = pgTable("teachers", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
-  document: varchar("document", { length: 20 }).notNull().unique(),
-  registration: varchar("registration", { length: 20 }).notNull().unique(),
+  department: text("department").notNull(),
+  birthDate: timestamp("birth_date", { withTimezone: true }).notNull(),
+  nationality: varchar("nationality", { length: 100 }).notNull(),
+  phone: varchar("phone", { length: 20 }).notNull(),
+  maritalStatus: varchar("marital_status", { length: 20 }).notNull(),
   createdAt: timestamp("created_at", { withTimezone: true }).notNull(),
   updatedAt: timestamp("updated_at", { withTimezone: true }).notNull(),
 });
